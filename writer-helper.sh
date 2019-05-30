0c8b620f7564144845c4026a300cf20e90ef4afd
Cassandra Lee Yieng <iamthegirlclaudia@yahoo.com>
2019-05-30 21:37:35 +0800

    A (dumb) responsive bash prompt
    To get me writing. I need interactivity to write.
    This saves all inputs to an external file.

---
+++ writer-helper.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+fname="TS_story.md"
+
+echo -e "\033[1mWelcome to your story writer! What happens in your story?\033[0m ($(($( wc -w <"$fname" ))) words)"
+read var
+echo $var >> $fname
+
+while :
+ do
+ echo -e "\033[1mWhat happens next?\033[0m ($(($( wc -w <"$fname" ))) words)" 
+ read var
+ echo $var >> TS_story.md
+done
