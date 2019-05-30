34701becfdb86143e0e4a42444a0baaa94fc28e4
Cassandra Lee Yieng <iamthegirlclaudia@yahoo.com>
2019-05-30 22:09:16 +0800

    Update writer-helper.sh

--- writer-helper.sh
+++ writer-helper.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+# This little bash script aims to make writers hammer out their first draft without thinking too much or rereading or editing/revising too early.
+
+# .md: markdown format (yes, URLs can be included)
 fname="the_story.md"
+
+# This will give a welcome prompt in bold :) and the word count of your work-in-progress
 
 echo -e "\033[1mWelcome to your story writer! What happens in your story?\033[0m ($(($( wc -w <"$fname" ))) words)"
 read var
 echo $var >> $fname
 
+# This will nag you in bold, like a little child eager to ask you more about your story
 while :
  do
  echo -e "\033[1mWhat happens next?\033[0m ($(($( wc -w <"$fname" ))) words)" 
