7faae847ea2b078c54bfb95aad2e541cb2aea673
Cassandra Lee Yieng <iamthegirlclaudia@yahoo.com>
2019-06-04 14:56:50 +0800

    Update start-at-the-end.sh

--- start-at-the-end.sh
+++ start-at-the-end.sh
@@ -16,7 +16,7 @@ echo $var >> $fname
 # (and yes, the word count is automatically updated every time you press ENTER)
 while :
  do
- echo -e "\033[1mWhy did it happen? (Challenge: Come up with 5 different answers.)\033[0m ($(($( wc -w <"$fname" ))) words)" 
+ echo -e "\033[1mWhy did it happen? (Challenge: Come up with 5 different answers, and decide which one(s) to explore.)\033[0m ($(($( wc -w <"$fname" ))) words)" 
  read var
  echo $var >> $fname
 done
